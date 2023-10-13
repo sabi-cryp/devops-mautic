48b79a42ee26af6f950421c002319f4107aaeb4c
sabrine kerchaoui <74612485+sabi-cryp@users.noreply.github.com>
2023-10-13 16:20:12 +0200

    Update create-mautic-client.sh

--- create-mautic-client.sh
+++ create-mautic-client.sh
@@ -1,49 +1,86 @@
 #!/bin/bash
 
-# Input: Client name, passed as a parameter to the script
+# Input: Client name
 CLIENT_NAME=$1
 
-# Define database and URL variables for the client
-DB_HOST="db-server"  # Database server hostname
-DB_USER="${CLIENT_NAME}_db_user"  # Unique database username for the client
-DB_PASSWORD="${CLIENT_NAME}_db_password"  # Unique database password for the client
-CLIENT_URL="http://10.10.204.6:8080/$CLIENT_NAME"  # Unique URL for the client
+# Step 1: Create a directory for the client
+CLIENT_DIR="/path/to/clients/$CLIENT_NAME"
+mkdir -p "$CLIENT_DIR"
 
-# Step 1: Create a Database for the Client
-# In this step, the script connects to the database server and creates a dedicated database for the client. The database name is derived from the client name.
+# Step 2: Create a Docker Compose file for the client
+DOCKER_COMPOSE_FILE="$CLIENT_DIR/docker-compose-$CLIENT_NAME.yml"
+cat <<EOL > "$DOCKER_COMPOSE_FILE"
+version: '3'
+services:
+  $CLIENT_NAME-mautic:
+    image: mautic/mautic:v4-apache
+    container_name: $CLIENT_NAME-mautic
+    ports:
+      - "8001:80"
+    volumes:
+      - ./mautic-data-$CLIENT_NAME:/var/www/html
+    environment:
+      - MAUTIC_DB_HOST=db
+      - MAUTIC_DB_NAME=$CLIENT_NAME
+      - MAUTIC_DB_USER=$CLIENT_NAME-user
+      - MAUTIC_DB_PASSWORD=$CLIENT_NAME-password
+EOL
 
-# mysql -u root -p -e "CREATE DATABASE $CLIENT_NAME;"
+# Step 3: Create a Kubernetes configuration file for the client
+K8S_CONFIG_FILE="$CLIENT_DIR/k8s-config-$CLIENT_NAME.yaml"
+cat <<EOL > "$K8S_CONFIG_FILE"
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: $CLIENT_NAME-mautic-deployment
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: $CLIENT_NAME-mautic
+  template:
+    metadata:
+      labels:
+        app: $CLIENT_NAME-mautic
+    spec:
+      containers:
+        - name: $CLIENT_NAME-mautic
+          image: mautic/mautic:v4-apache
+          ports:
+            - containerPort: 80
+  # Define other Kubernetes resources (e.g., Service) if needed
+EOL
 
-# Step 2: Configure Mautic for the Client
-# This is a placeholder for configuring Mautic for the client. You would set up Mautic settings, templates, and other configurations specific to the client's needs. This might involve copying configuration files, applying templates, and more.
+# Step 4: Create a Kubernetes service configuration for the client
+K8S_SERVICE_FILE="$CLIENT_DIR/k8s-service-$CLIENT_NAME.yaml"
+cat <<EOL > "$K8S_SERVICE_FILE"
+apiVersion: v1
+kind: Service
+metadata:
+  name: $CLIENT_NAME-mautic-service
+spec:
+  selector:
+    app: $CLIENT_NAME-mautic
+  ports:
+    - protocol: TCP
+      port: 80
+      targetPort: 80
+  type: NodePort  # Use the appropriate service type for your setup
+EOL
 
-# Step 3: Create a Virtual Host or URL Routing
-# In this step, the script creates a virtual host or sets up URL routing to map the client's URL to the Mautic instance. This allows clients to access Mautic using their dedicated URL.
-
-# Example: Creating an Apache Virtual Host
-# This step can involve editing the Apache configuration to create a virtual host for the client. This is an example of how you might do it:
-
-# Create a configuration file for the client's virtual host
-cat <<EOF > /etc/apache2/sites-available/${CLIENT_NAME}.conf
-<VirtualHost *:80>
-    ServerAdmin webmaster@localhost
-    ServerName $CLIENT_NAME.mautic-server.com
-    DocumentRoot /var/www/html/$CLIENT_NAME
-
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-    # Additional configuration specific to the client can be added here
-</VirtualHost>
-EOF
-
-# Enable the virtual host configuration
-a2ensite ${CLIENT_NAME}.conf
-
-# Reload Apache to apply the changes
-systemctl reload apache2
-
-# End of the Script
-# The script ends by echoing a success message to indicate that the client has been created.
+# Step 5: Additional client-specific setup
+# Add more steps here to perform any other client-specific configurations
 
 echo "Client $CLIENT_NAME has been created successfully."
+
+# Step 6: Deploy the client using Docker Compose
+docker-compose -f "$DOCKER_COMPOSE_FILE" up -d
+
+# Step 7: Push the Docker image to Nexus
+docker login -u skerchaoui -p skerchaoui23#
+docker tag $CLIENT_NAME-mautic:latest $DOCKER_REGISTRY_URL/$CLIENT_NAME-mautic:latest
+docker push $DOCKER_REGISTRY_URL/$CLIENT_NAME-mautic:latest
+
+# Step 8: If you want to deploy to Kubernetes, apply the Kubernetes configuration
+kubectl apply -f "$K8S_CONFIG_FILE"
+kubectl apply -f "$K8S_SERVICE_FILE"
