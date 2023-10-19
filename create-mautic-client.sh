42463119b8bd11cee60b680913491b8a44f8c4e1
sabrine kerchaoui <74612485+sabi-cryp@users.noreply.github.com>
2023-10-19 12:01:01 +0200

    Update create-mautic-client.sh

--- create-mautic-client.sh
+++ create-mautic-client.sh
@@ -112,10 +112,10 @@ server {
 
     location / {
         proxy_pass http://10.10.204.7:${PORT_NUMBER};
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        #proxy_set_header Host $host;
+        #proxy_set_header X-Real-IP $remote_addr;
+        #proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+       # proxy_set_header X-Forwarded-Proto $scheme;
     }
 
     error_log /var/log/nginx/error.log;
